6cf400b8a376219985b41d5e68074c9ca72c7141
luisertz <luisertz@gmail.com>
2020-07-14 12:41:52 -0500

    primer commit

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const Task = require('../model/task');
 const bodyParser = require('body-parser')
-const Swal = require('sweetalert2')
 const bodyParserJSON = bodyParser.json();
 const bodyParserURLEncoded = bodyParser.urlencoded({ extended: true });
 
